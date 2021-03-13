60eed6e038234dc8a1204b435041314f9f803bce
Gasper3 <trzecik65@gmail.com>
2021-03-13 17:08:35 +0100

    Handle passed filenames

--- merge.py
+++ merge.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 import PyPDF2
 import os
 import sys
-from datetime import datetime
+
 
 arguments = sys.argv[1:]
 if not arguments:
@@ -12,22 +14,26 @@ path = arguments[0]
 if not path.endswith('/'):
     path += '/'
 
-pdfFiles = [filename for filename in os.listdir(path) if filename.endswith('.pdf')]
+pdfFilesArg = sys.argv[2:]
+pdfFiles = pdfFilesArg or [filename for filename in os.listdir(path) if filename.endswith('.pdf')]
+pdfFiles.sort(key=str.lower)
 
 if not pdfFiles:
-    print(f'There are 0 pdf files in: {path}')
+    print(f'There are no pdf files in: {path}')
     exit(1)
-
-pdfFiles.sort(key=str.lower)
 
 pdfWriter = PyPDF2.PdfFileWriter()
 
-for filename in pdfFiles:
-    pdfFileObj = open(path + filename, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    for pageNum in range(0, pdfReader.numPages):
-        pageObj = pdfReader.getPage(pageNum)
-        pdfWriter.addPage(pageObj)
+try:
+    for filename in pdfFiles:
+        pdfFileObj = open(path + filename, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        for pageNum in range(0, pdfReader.numPages):
+            pageObj = pdfReader.getPage(pageNum)
+            pdfWriter.addPage(pageObj)
+except FileNotFoundError:
+    print(f'There are no file {filename} in {path}')
+    exit(1)
 
 filename = datetime.now().strftime('%Y-%m-%d_%H%M%S')
 pdfOutput = open(path + f'{filename}.pdf', 'wb')
