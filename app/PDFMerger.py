a33d15eef65db30a4d530fc4c5dad605b4d6101b
Gasper3 <trzecik65@gmail.com>
2021-10-07 22:27:06 +0200

    Refactor. Joining file name with .pdf when user pass name without file extension

--- app/PDFMerger.py
+++ app/PDFMerger.py
@@ -10,21 +10,14 @@ class Merger:
         self.path = path
         self.pdf_files = pdf_files
 
-    def _get_files(self):
-        pdf_files = self.pdf_files or [filename for filename in os.listdir(self.path) if filename.endswith('.pdf')]
-        pdf_files.sort(key=str.lower)
-
-        if not pdf_files:
-            print(f'There are no pdf files in: {self.path}')
-            exit(1)
-
-        return pdf_files
-
     def merge_files(self):
         try:
             for filename in self._get_files():
+                if not filename.endswith(".pdf"):
+                    filename += ".pdf"
                 pdf_file = open(self.path + filename, 'rb')
                 pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+
                 for nr in range(0, pdf_reader.numPages):
                     page = pdf_reader.getPage(nr)
                     self.pdfWriter.addPage(page)
@@ -36,3 +29,13 @@ class Merger:
         pdf_output = open(self.path + f'{filename}.pdf', 'wb')
         self.pdfWriter.write(pdf_output)
         pdf_output.close()
+
+    def _get_files(self) -> list[str]:
+        pdf_files = self.pdf_files or [filename for filename in os.listdir(self.path) if filename.endswith('.pdf')]
+        pdf_files.sort(key=str.lower)
+
+        if not pdf_files:
+            print(f'There are no pdf files in: {self.path}')
+            exit(1)
+
+        return pdf_files
