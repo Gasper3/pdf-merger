03637f19fd451b4a4b849690e55b105423780c5e
Gasper3 <trzecik65@gmail.com>
2022-04-22 10:19:16 +0200

    Removed commented lines

--- src/pdfmerge2/app.py
+++ src/pdfmerge2/app.py
@@ -4,12 +4,7 @@ import os
 from .exceptions import FilesNotFoundInDirectoryError, PathNotExistsError
 from .merger import Merger
 
-is_debug = os.getenv("MERGER_DEBUG")
-# logger = logging.Logger(__name__, level=logging.DEBUG if is_debug else logging.ERROR)
-# log_formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s")
-# file_handler = logging.FileHandler(Path("log/app.log"), mode='a' if not is_debug else 'w')
-# file_handler.setFormatter(log_formatter)
-# logger.addHandler(file_handler)
+is_debug = os.getenv("PDFMERGER2_DEBUG")
 
 
 def run():
@@ -25,16 +20,12 @@ def run():
     try:
         merger.merge_files()
     except PathNotExistsError as e:
-        # logger.error(e)
         print("Provided path does not exist" if not is_debug else e)
     except FilesNotFoundInDirectoryError as e:
-        # logger.error(e)
         print("Files not found" if not is_debug else e)
     except FileNotFoundError as e:
-        # logger.error(e)
         print("File not found" if not is_debug else e)
     except Exception as e:
-        # logger.error(e)
-        print("Error occured" if not is_debug else e)
+        print("Error occurred" if not is_debug else e)
     else:
         print(f"Files merged into: {merger.merged_file_name}")
